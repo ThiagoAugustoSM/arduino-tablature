8e0ba22ee4dba4c6a430270f5e497bb533023b85
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-02-19 16:55:24 +0000

    Removing default url to make it clear when user dont pass url param

--- index.js
+++ index.js
@@ -19,43 +19,41 @@ var url;
 if(args.url != undefined){
   url = args.url;
   console.log('requesting from:' + args.url)
+  // Request to get the html from cifraclub's website
+  request(url, function(error, response, html){
+  
+    if(!error){
+      var $ = cheerio.load(html);
+  
+      // In the webpage the capo text is like:
+      // 2ª casa
+      var capo = $("#cifra_capo").children().contents().text();
+      if(capo.length > 0){
+        capo = parseInt(capo.split('ª')[0]);
+      }else{
+        capo = 0;
+      }
+  
+      $(".tablatura").each(function(){
+        var tab = $(this).text();
+        tabInNotes.push(tablature.filterTab(tab, capo));
+      });
+  
+      var arduinoFile = makeFile.generateFile(tabInNotes);
+  
+      // Saving file in the same directory
+      fs.writeFile('music/music.ino', arduinoFile, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+  
+    }else{
+      console.log("ERROR");
+    }
+  })
 }
 else{
-  url = 'https://www.cifraclub.com.br/natiruts/andei-so/'
-  console.log('no arguments detected')
+  console.log('You must pass a url in a param, example: --url=https://www.cifraclub.com.br/natiruts/andei-so/')
 }
 
 var tabInNotes = [];
-
-// Request to get the html from cifraclub's website
-request(url, function(error, response, html){
-
-  if(!error){
-    var $ = cheerio.load(html);
-
-    // In the webpage the capo text is like:
-    // 2ª casa
-    var capo = $("#cifra_capo").children().contents().text();
-    if(capo.length > 0){
-      capo = parseInt(capo.split('ª')[0]);
-    }else{
-      capo = 0;
-    }
-
-    $(".tablatura").each(function(){
-      var tab = $(this).text();
-      tabInNotes.push(tablature.filterTab(tab, capo));
-    });
-
-    var arduinoFile = makeFile.generateFile(tabInNotes);
-
-    // Saving file in the same directory
-    fs.writeFile('music/music.ino', arduinoFile, function (err) {
-      if (err) throw err;
-      console.log('Saved!');
-    });
-
-  }else{
-    console.log("ERROR");
-  }
-})
