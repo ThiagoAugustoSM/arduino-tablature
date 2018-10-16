690ebd64b0012ad16c4b31110b1c761987101967
Giancarlo Giuffra <giancarlo.giuffra.moncayo@gmail.com>
2018-10-16 19:03:46 +0200

    e-chords.scraper: refactor test

--- test/e-chords.scraper.js
+++ test/e-chords.scraper.js
@@ -1,24 +1,28 @@
 const assert = require('assert');
 const scraper = require('../src/scraper.js');
 
-describe('scraper', function() {
-
-  describe('e-chords', function() {
-
-    it('should get one tab', function() {
-      const tab = `E|-------|-0-----------------3----0---3-|-------| 
+const tab = `E|-------|-0-----------------3----0---3-|-------| 
 B|-1---3-|-------3---1----3-------------|---3---| 
 G|-------|------------------------------|-------| 
 D|-------|------------------------------|-------| 
 A|-------|-0----------------------------|-------| 
 E|-------|----------------3-------------|---3---|`;
 
-      const html = `
-      <pre id="core">
-${tab}
+function eChordsHtml(content){
+    return `
+    <pre id="core">
+    ${content}
 
-      </pre>
-      `;
+    </pre>
+    `;
+}
+
+describe('scraper', function() {
+
+  describe('e-chords', function() {
+
+    it('should get one tab', function() {
+      const html = eChordsHtml(tab);
 
       let song = scraper.getSong('e-chords', html);
       assert.equal(song.tabs.length, 1);
